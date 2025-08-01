aae5f52cf9b3b38de3533473eed5e57443bae307
Manish <manish2003sharma@gmail.com>
2025-08-01 21:40:04 +0530

    Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import Home from './pages/Home';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
     return (
-        <Home />
+        // <Home />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} /> */}
+        </Routes>
     );
 };
 
