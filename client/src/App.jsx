2c068d9b586d479958ea5b08823dcc997a35d0df
Manish <manish2003sharma@gmail.com>
2025-08-09 15:41:58 +0530

    Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Home from './pages/Home';
 import { Route, Routes } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
 
 const App = () => {
     return (
         // <Home />
         <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} /> */}
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
         </Routes>
     );
 };
