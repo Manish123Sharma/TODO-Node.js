c9f01a3ac76cf17786dfc2a1435c0e4a76a2e57f
Manish <manish2003sharma@gmail.com>
2025-08-11 18:18:47 +0530

    server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 import dotenv from 'dotenv';
 import cors from 'cors';
+import authRoutes from "./routes/authRoutes.js";
+import todoRoutes from './routes/todoRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -9,12 +11,15 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
+app.use('/api/todos', todoRoutes);
+
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 })
-.then(() => console.log('MongoDB Connected'))
-.catch((err) => console.error('MongoDB Connection Error:', err));
+    .then(() => console.log('MongoDB Connected'))
+    .catch((err) => console.error('MongoDB Connection Error:', err));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
