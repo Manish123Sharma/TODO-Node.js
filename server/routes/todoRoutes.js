6f60e5535e25d74b716763e8067f0c811998115c
Manish <manish2003sharma@gmail.com>
2025-08-11 22:23:16 +0530

    routes

--- server/routes/todoRoutes.js
+++ server/routes/todoRoutes.js
@@ -1,21 +1,43 @@
-import express from 'express';
-import protect from '../middleware/authMiddleware.js';
-import {
-    getTodos,
-    createTodo,
-    updateTodo,
-    deleteTodo
-} from '../controllers/todoController.js';
+// import express from "express";
+// import Todo from "../models/Todo.js";
+// import protect from "../middleware/authMiddleware.js";
+const express = require("express");
+// const { createTodo, getTodos, updateTodo, deleteTodo } = require("../controllers/todoController");
+const Todo = require('../models/Todo')
+const protect = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
-// All routes are protected (require valid JWT)
-router.route('/')
-    .get(protect, getTodos)
-    .post(protect, createTodo);
+// Create task
+router.post("/", protect, async (req, res) => {
+    const todo = new Todo({
+        text: req.body.text,
+        user: req.user.id
+    });
+    const saved = await todo.save();
+    res.json(saved);
+});
 
-router.route('/:id')
-    .put(protect, updateTodo)
-    .delete(protect, deleteTodo);
+// Get todos
+router.get("/", protect, async (req, res) => {
+    const todos = await Todo.find({ user: req.user.id });
+    res.json(todos);
+});
 
-export default router;
+// Update task
+router.put("/:id", protect, async (req, res) => {
+    const todo = await Todo.findOneAndUpdate(
+        { _id: req.params.id, user: req.user.id },
+        { completed: req.body.completed },
+        { new: true }
+    );
+    res.json(todo);
+});
+
+// Delete task
+router.delete("/:id", protect, async (req, res) => {
+    await Todo.findOneAndDelete({ _id: req.params.id, user: req.user.id });
+    res.json({ message: "Todo deleted" });
+});
+
+module.exports = router;
